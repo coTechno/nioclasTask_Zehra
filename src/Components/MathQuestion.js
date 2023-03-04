ccd756687ae84a7054a14575b010a884210b89f5
Zehra Khan <93855985+coTechno@users.noreply.github.com>
2023-03-04 16:46:23 +0530

    Update MathQuestion.js

--- src/Components/MathQuestion.js
+++ src/Components/MathQuestion.js
@@ -1,36 +1,32 @@
-import React, { useEffect, useState } from 'react'
-import { MathJax } from 'better-react-mathjax/MathJax';
-import { Form } from 'react-bootstrap'
+import React, { useEffect, useState } from 'react';
+import { MathJax } from 'better-react-mathjax';
+import { Form } from 'react-bootstrap';
 import CircularProgress from '@mui/material/CircularProgress';
-import './MathQuestion.css'
+import './MathQuestion.css';
 
 const MathQuestion = () => {
-    const [isLoading, setIsloading] = useState(true)
-    const questionID = ['AreaUnderTheCurve_901', 'BinomialTheorem_901', 'DifferentialCalculus2_901']
-    const [id, setId] = useState(0)
-    const [title, setTitle] = useState('')
-    const [question, setQuestion] = useState('')
-   let url = `https://0h8nti4f08.execute-api.ap-northeast-1.amazonaws.com/getQuestionDetails/getquestiondetails?QuestionID=${questionID[id]}`
+    const [isLoading, setIsloading] = useState(true);
+    const questionID = ['AreaUnderTheCurve_901', 'BinomialTheorem_901', 'DifferentialCalculus2_901'];
+    const [id, setId] = useState(0);
+    const [title, setTitle] = useState('');
+    const [question, setQuestion] = useState('');
+    let url = `https://0h8nti4f08.execute-api.ap-northeast-1.amazonaws.com/getQuestionDetails/getquestiondetails?QuestionID=${questionID[id]}`;
     useEffect(() => {
         const fetchApi = async () => {
-            const fetchedQues = await fetch(url)
-            const data = await fetchedQues.json()
+            const fetchedQues = await fetch(url);
+            const data = await fetchedQues.json();
             setQuestion(data[0].Question);
             setTitle(data[0].ChapterID);
-            setIsloading(false)
-        }
-        fetchApi()
-    }, [id, url])
+            setIsloading(false);
+        };
+        fetchApi();
+    }, [id, url]);
     return (
         <div className={`container mt-5 ${isLoading ? 'loader' : ''}`}>
             {isLoading ? <CircularProgress color="inherit" />
                 : <div className='content'>
                     <h2><strong>{title}</strong></h2>
-                    <span>
-                        <MathJax>
-                            {question}
-                        </MathJax>
-                    </span>
+                    <MathJax>{question}</MathJax>
                     <div>
                         <Form.Group className="mb-3">
                             <Form.Label>Code Editor : </Form.Label>
@@ -43,7 +39,7 @@ const MathQuestion = () => {
                     </div>
                 </div>}
         </div>
-    )
-}
+    );
+};
 
-export default MathQuestion
+export default MathQuestion;
